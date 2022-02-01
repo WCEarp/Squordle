8f5fb53ee4ccd8f984be27ce0e3045572521f3e3
WCEarp <wyattcodyearp@gmail.com>
2022-01-31 20:04:12 -0600

    Postgress added

--- wordlebot.py
+++ wordlebot.py
@@ -1,41 +1,61 @@
 import os
 import discord
-import wordleparser
 import datetime
+import psycopg2
+import settings
 
 from dotenv import load_dotenv
 from discord.ext import commands
 from email.errors import MessageError
 from unicodedata import name
+from wordleparser import get_points, parse, get_scoreboard
 
+bot = commands.Bot(command_prefix='!')
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
-CID = 936621404651155550
-
-client = commands.Bot(command_prefix='!')
-scoreboard = {}
-
-@client.event
+# Default Events
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
-    start_date = datetime.datetime.now() - datetime.timedelta(days=27)
-    channel = client.get_channel(CID)
-    messages = await channel.history(limit=500, after=start_date).flatten()
-    for message in messages:
-        if message.content.startswith("Wordle"):
-            parser.parse(message.author.name, message.content)
+    #try:
+        #connect to database
+    print(f'{bot.user} has connected to Discord!')
 
-@client.event
+@bot.event
 async def on_message(message):
     if message.content.startswith("Wordle"):
-        parser.parse(message.author.name, message.content)
-    await client.process_commands(message)
+        player = message.author.name
+        day, score = parse(message.content)
+        points = get_points(score)
+        cid = message.TextChannel.Guild.id
+        add_to_db(cid, player, score, points, day)
+    await bot.process_commands(message)
 
-@client.command()
+# Custom Commands
+@bot.command()
 async def score(ctx):
-    await ctx.send("```\n{}```".format(parser.get_scoreboard()))
+    await ctx.send("```\n{}```".format(get_scoreboard()))
 
-parser = wordleparser.WordleParser()
-client.run(TOKEN)
+@bot.command()
+async def add(ctx):
+    pass
+
+def add_to_db(cid, player, score, points, day):
+
+
+################################################################################
+if __name__ == "__main__":
+    try:
+        # Connect to DB
+        connection = psycopg2.connect(user=settings.DATABASE_USER,
+                                      password=settings.DATABASE_PSWD,
+                                      host=settings.DATABASE_HOST,
+                                      port=settings.DATABASE_PORT,
+                                      database=settings.DATABASE_NAME)
+        # Create a cursor to perform database operations
+        cursor = connection.cursor()
+    except Exception as error:
+        print( "Error connecting to DB: ", error)
+    finally:
+        if (connection):
+            cursor.close()
+            connection.close()
+            bot.run(settings.DISCORD_TOKEN)
--- wordleparser.py
+++ wordleparser.py
@@ -1,66 +1,64 @@
-import json
+import settings
+import psycopg2
 import atexit
 from typing_extensions import Self
-from tabulate import tabulate
+#from tabulate import tabulate
 
-class WordleParser:
+def parse(message):
+    tmp = message.split("\n")[0].split(" ")
+    return tmp[1], tmp[2]
 
-    def __init__(self):
-        self.data = [] 
-        with open('data.json', 'r') as data_file:
-            self.data = json.load(data_file)
+def get_scoreboard():
+    raise NotImplemented
 
-    def parse(self, player, message):
-        tmp = message.split("\n")[0].split(" ")
-        day = tmp[1]
-        score = tmp[2]
-        if not player in self.data["scoreboard"]["players"]:
-            self.data["scoreboard"]["players"][player] = {
-                ["wordles"][day]: score,
-                "current_score": self.get_points(score)
-            }
-        else:
-            self.data["scoreboard"]["players"][player]["wordles"][day] = score
-            self.data["scoreboard"]["players"][player]["current_score"] += self.get_points(score)
-    
-    def get_scoreboard(self):
-        table = []
-        for player in self.data["scoreboard"]["players"]:
-            table.append((player, self.data["scoreboard"]["players"][player]["current_score"]))
-        return tabulate(table.sort(), ["Player", "Score"], tablefmt="grid")
-
-    def get_points(self, score):
+def get_points(score):
+    points = 0
+    if (score.split("/")[0] == "x"):
         points = 0
-        if (score.split("/")[0] == "x"):
-            points = 0
-        elif (score.split("/")[0] == "1"):
-            points = 6
-        elif (score.split("/")[0] == "2"):
-            points.split("/")[0] = 5
-        elif (score.split("/")[0] == "3"):
-            points = 4
-        elif (score.split("/")[0] == "4"):
-            points = 3
-        elif (score.split("/")[0] == "5"):
-            points = 2
-        elif (score.split("/")[0] == "6"):
-            points = 1
+    elif (score.split("/")[0] == "1"):
+        points = 6
+    elif (score.split("/")[0] == "2"):
+        points.split("/")[0] = 5
+    elif (score.split("/")[0] == "3"):
+        points = 4
+    elif (score.split("/")[0] == "4"):
+        points = 3
+    elif (score.split("/")[0] == "5"):
+        points = 2
+    elif (score.split("/")[0] == "6"):
+        points = 1
 
-        return points
-
-    def close(self):
-        with open('data.json', 'w') as fp:
-            json.dump(self.data, fp, indent=2)
-
-    def exit_handler(self):
-        self.close()
+    return points
 
 ########################################################################################################
 
 if __name__ == "__main__":
-    debug = WordleParser()
     #print(debug.data["scoreboard"]["players"][0]["Squitward"])
-    name = "Squitward"
+    name = 'Squitward'
     msg = "Wordle 223 4/6\n\n⬛⬛⬛⬛⬛\n⬛⬛🟨⬛⬛\n⬛🟨🟨⬛⬛\n🟨🟩🟨⬛🟩\n🟩🟩🟩🟩🟩"
-    debug.parse(name, msg)
-    print(debug.get_scoreboard())
+    cid = 1234567890
+    day, score = parse(msg)
+    points = get_points(score)
+    try:
+        # Connect to DB
+        connection = psycopg2.connect(user=settings.DATABASE_USER,
+                                      password=settings.DATABASE_PSWD,
+                                      host=settings.DATABASE_HOST,
+                                      port=settings.DATABASE_PORT,
+                                      database=settings.DATABASE_NAME)
+        # Create a cursor to perform database operations
+        cursor = connection.cursor()
+        # Print PostgreSQL details
+        print("PostgreSQL server information")
+        print(connection.get_dsn_parameters(), "\n")
+        # Executing a SQL query
+        cursor.execute('INSERT INTO scoreboard (channel_id,player,score,points,wordle_number) \
+                        VALUES (%s, %s, %s, %s, %s);', (cid, 'Squitward', '5/6', 2, 225))
+        # Fetch result
+        connection.commit()
+    except Exception as error:
+        print( "Error connecting to DB: ", error)
+    finally:
+        if (connection):
+            cursor.close()
+            connection.close()
